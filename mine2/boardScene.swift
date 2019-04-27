987d3250fe93ab3abf2eb200a50f2b50ff3bde21
Adam Piekarski <adpiekar@lcs-209-130.syr.edu>
2019-04-27 00:17:46 -0400

    Add tile map

--- mine2/boardScene.swift
+++ mine2/boardScene.swift
@@ -11,7 +11,30 @@ import SpriteKit
 
 class boardScene: SKScene {
     //var board: Board
+    //var camera: SKCameraNode?
     
+    override func didMove(to view: SKView) {
+        let camera = SKCameraNode(fileNamed: "cam")
+    }
+    
+    @objc func handlePinch(pinchGesture: UIPinchGestureRecognizer) {
+        if pinchGesture.state == .began || pinchGesture.state == .changed {
+            
+            let currentScale: CGFloat = (camera!.xScale)
+            let minScale: CGFloat = 0.5
+            let maxScale: CGFloat = 2.0
+            let zoomSpeed: CGFloat = 0.5
+            var deltaScale = pinchGesture.scale
+            
+            deltaScale = ((deltaScale - 1) * zoomSpeed) + 1
+            deltaScale = min(deltaScale, maxScale / currentScale)
+            deltaScale = max(deltaScale, minScale / currentScale)
+            
+            camera?.xScale = deltaScale
+            camera?.yScale = deltaScale
+        }
+        
+    }
     /*(init(b: Board) {
         board = b
         super.init()
